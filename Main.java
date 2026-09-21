8f1a270f46aeaa48457aa559f6c5d34c441843bf
Misha981-tech <239153351+Misha981-tech@users.noreply.github.com>
2026-09-21 14:29:33 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
   public static void main(String[] args) {
+    //Display welcome message
      System.out.println("===== Welcome to Home Comfort Furniture! =====\n");
+
+    //Create an array of items for sale and their corresponding quantities 
      Item[]items = {
      new FlatDiscountItem("Bed", 350000.00, 2, 20000),
      
@@ -11,6 +14,8 @@ public class Main {
             1,
             2
         };
+    
+    //Display available items
       System.out.println("Available Items:");
 
       for(int i =0;i<items.length;i++){
@@ -21,10 +26,12 @@ public class Main {
 
       System.out.println("\n================ RECEIPT ================\n");
 
+      //Calculate the total for each item    
       for(int i= 0;i<items.length;i++){
         double subtotal = items[i].calculateTotal(quantities[i]);
-        System.out.println(items[i].getName()+ "x"+ quantities[i]+"= UGX"+subtotal);
+        System.out.println(items[i].getName()+ " UGX "+item[i].getPrice()+ " x "+ quantities[i]+" = UGX"+subtotal);
 
+        //Get the discount  note for the item and print it if it's not empty
         String discountNote = items[i].getDiscountNote(quantities[i]);
         if(!discountNote.isEmpty()){
           System.out.println("(" +discountNote + ")");
