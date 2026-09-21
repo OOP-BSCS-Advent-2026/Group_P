3b97baea53b8d6edc293b09340a6a748955b8c8c
CommandLine-Protocol <231300314+CommandLine-Protocol@users.noreply.github.com>
2026-09-21 12:10:02 +0300

    format constructor, add getters, and clean up calculateTotal logic

--- PercentDiscountItem.java
+++ PercentDiscountItem.java
@@ -1,22 +1,28 @@
 public class PercentDiscountItem extends Item {
 
-    private int threshold;
+    private int minQuantity;
     private double percentOff;
 
-    public PercentDiscountItem(String name, double price,
+    public PercentDiscountItem(String name, double price, int minQuantity, double percentOff) {
+        super(name, price);
+        this.minQuantity = minQuantity;
+        this.percentOff = percentOff;
+    }
 
-        int threshold, double percentOff) {
-            super(name, price);
-            this.threshold = threshold;
-            this.percentOff = percentOff;
+    public int getMinQuantity() {
+        return minQuantity;
+    }
+
+    public double getPercentOff() {
+        return percentOff;
+    }
+
+    @Override
+    public double calculateTotal(int quantity) {
+        double subtotal = super.calculateTotal(quantity);
+        if (quantity >= minQuantity) {
+            subtotal = subtotal * (1 - percentOff / 100.0);
         }
-        
-        @Override
-        public double calculateTotal(int quantity) {
-            double subtotal = super.calculateTotal(quantity);
-            if (quantity >= threshold) {
-                subtotal = subtotal * (1 - percentOff / 100.0);
-            }
         return subtotal;
-        }
+    }
 }
